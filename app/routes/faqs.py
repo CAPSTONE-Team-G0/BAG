416e59f5e83bc9549707329e99d7c1ad816c947d
Joey Ackerman-Lowery <ackermaj5301@student.faytechcc.edu>
2026-05-01 17:58:53 -0400

    Delete app directory

--- app/routes/faqs.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from app.auth import login_required
-
-bp = Blueprint("faqs", __name__)
-
-@bp.route("/faqs")
-@login_required
-def faqs():
-    return render_template("faqs.html")
